b35e1beb0a326ca841066532c782ccd738c1eea1
nandanikoundal <109542801+nandanikoundal@users.noreply.github.com>
2022-07-28 10:05:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# relativelayout
+ assignment
---
+++ app/src/main/java/com/example/relativelayout/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.relativelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.relativelayout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding
+        setContentView(binding.root)
+        binding.
+    }
+}
